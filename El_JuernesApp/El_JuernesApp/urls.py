6f7482270eedb62b9b6b55daed8b095e1a579028
adricasi <adriator6@gmail.com>
2018-03-29 11:30:29 +0200

    la url per defecte "127.0.0.1:8000/" és la pàgina principal

--- El_JuernesApp/El_JuernesApp/urls.py
+++ El_JuernesApp/El_JuernesApp/urls.py
@@ -22,6 +22,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^accounts/', include('Accounts.urls')),
-    path('home/', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
     path('AFE/', include('AfeNews.urls')),
 ]
