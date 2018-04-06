641087073de81434a14171a780f033480e68798e
Ricard Zapater Muñoz <ricard_zm@hotmail.com>
2018-04-06 10:03:37 +0200

    Models actualitzats amb __str__

--- El_JuernesApp/Copywriter/models.py
+++ El_JuernesApp/Copywriter/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Article(models.Model):
     slug = models.CharField(max_length=140)
     title = models.CharField(max_length=140)
@@ -9,3 +7,6 @@ class Article(models.Model):
     body = models.TextField()
     assigned = models.CharField(max_length=140, default="Cap redactor assignat")
     priority = models.CharField(max_length=140, default='')
+
+    def __str__(self):
+        return 'Article: ' + self.title
