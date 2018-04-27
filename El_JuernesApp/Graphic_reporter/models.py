95682995cff71e1bb8e8148413be237f5823da15
Ricard Zapater Muñoz <ricard_zm@hotmail.com>
2018-04-27 09:06:56 +0200

    Categories

--- El_JuernesApp/Graphic_reporter/models.py
+++ El_JuernesApp/Graphic_reporter/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
+POLITICS = "Política"
+SPORTS = "Esports"
+NATURE = "Natura"
+MUSIC = "Musica"
+ART = "Art"
+VEHICLE = "Vehicle"
+OTHER = "Altres"
 
 
 class Image(models.Model):
-    description = models.CharField(max_length=140)
+    CATEGORIES = (
+        (POLITICS, "Política"),
+        (SPORTS, "Esports"),
+        (NATURE, "Natura"),
+        (MUSIC, "Musica"),
+        (ART, "Art"),
+        (VEHICLE, "Vehicle"),
+        (OTHER, "Altres"),
+    )
+
+    name = models.CharField(max_length=140)
     image = models.ImageField(upload_to='')
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=30, choices=CATEGORIES, null=True)
+
 
     def __str__(self):
-        return 'Image: ' + self.description
+        return 'Image: ' + self.name
