2729bd55e131bde13ef25fed6c28b5e7ad82c2ca
Ferran Martinez <fmp4@alumnes.udl.cat>
2018-05-11 13:34:11 +0200

    Llista de noticies acceptades, noticia acceptada individual, pantalla de maquetació

--- El_JuernesApp/Layout_designer/urls.py
+++ El_JuernesApp/Layout_designer/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls import url
 from django.urls import path
 
 from Layout_designer import views
 
 urlpatterns = [
+    url(r'^layout/(?P<slug>[\w-]+)/$',views.maquetar,name ='maquetar'),
+    url(r'^noticies_assignades/accepted/(?P<slug>[\w-]+)/$', views.new_details, name='new_layout'),
     path('noticies_assignades/', views.news_assigned, name="ld_assigned_news"),
 
 ]
--- El_JuernesApp/Layout_designer/views.py
+++ El_JuernesApp/Layout_designer/views.py
@@ -1,8 +1,34 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from AfeNews.models import New
+from Copywriter.models import Article
 
+
+def getNewContext(slug):
+    context = {}
+    context['new'] = New.objects.get(slug=slug)
+    context['article'] = Article.objects.get(slug=slug)
+    return context
 
 def news_assigned(request):
     template = 'Layout_designer/assigned_news.html'
-    context = None
+    context = {'Acceptades': New.objects.filter(state="Acceptat")}
 
     return render(request, template, context)
+
+def new_details(request,slug):
+    context = getNewContext(slug)
+
+    template = 'Layout_designer/layout_new.html'
+    return render(request, template, context)
+
+
+
+def maquetar(request,slug):
+    context = getNewContext(slug)
+
+    template = 'Layout_designer/layout_action.html'
+    return render(request,template,context)
+
+
+
