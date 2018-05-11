ec2951aabf5bcffe0c65e679acdeb7bdd034f0e5
Ricard Zapater Muñoz <ricard_zm@hotmail.com>
2018-05-11 18:51:04 +0200

    Llistat de notícies a punt de vèncer el termini

--- El_JuernesApp/AfeNews/views.py
+++ El_JuernesApp/AfeNews/views.py
@@ -1,14 +1,27 @@
 import codecs
-import datetime
 import json
 from urllib.request import urlopen
 
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.utils import timezone
 from django.views import generic
 
 from Accounts.models import User_profile
 from AfeNews.models import New, Author
+
+
+def set_limit_date(priority):
+    if priority == 'alta':
+        limit = timezone.now() + timezone.timedelta(hours=1)
+
+    elif priority == 'mitjana':
+        limit = timezone.now() + timezone.timedelta(days=1)
+
+    else:
+        limit = timezone.now() + timezone.timedelta(days=30)
+
+    return limit
 
 
 def Afe_News_List(request):
@@ -24,7 +37,7 @@ def Afe_News_List(request):
         new_obj.priority = prioritat
         new_obj.state = "Assignada"
 
-        new_obj.assignation_date = datetime.datetime.now()
+        new_obj.limit_date = set_limit_date(new_obj.priority)
 
         new_obj.save()
 
@@ -33,24 +46,25 @@ def Afe_News_List(request):
             "new": New.objects.get(slug=name[0])
         }
         return render(request, template, context)
+
     else:
         template = 'Home_News.html'
-        json_data = None
+    json_data = None
 
-        try:
-            user = User.objects.get(username=request.user.username)
-            rol = user.user_profile.role
+    try:
+        user = User.objects.get(username=request.user.username)
+        rol = user.user_profile.role
 
-            if rol == "Head_copywriter":
-                template = 'AfeNews/AfeNewsList.html'
+        if rol == "Head_copywriter":
+            template = 'AfeNews/AfeNewsList.html'
 
-            context = {
-                "articles": get_json_AFE_news(),
-            }
-        except:
-            template = 'Home_News.html'
+        context = {
+            "articles": get_json_AFE_news(),
+        }
+    except:
+        template = 'Home_News.html'
 
-        return render(request, template, context)
+    return render(request, template, context)
 
 
 def get_json_AFE_news():
