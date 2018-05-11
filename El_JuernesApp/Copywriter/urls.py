f0365abfd5ff405c3c26b045fda730a12f1e134a
adricasi <adriator6@gmail.com>
2018-05-11 22:00:08 +0200

    El redactor accedeix a un article concret amb comentaris.

--- El_JuernesApp/Copywriter/urls.py
+++ El_JuernesApp/Copywriter/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^request_send/$', views.send_request, name='request_send'),
     url(r'^send_article/$', views.send_new, name='send'),
     url(r'^assigned/(?P<slug>[\w-]+)/$', views.new_copywriter.as_view(), name='new_copywriter'),
-    path('', views.News_assigned, name="assigned"),
+    url(r'^Revisions/(?P<slug>[\w-]+)/$', views.Review_new.as_view(), name='review_new'),
     url(r'^Revisions/$', views.News_review, name='revisions'),
+    path('', views.News_assigned, name="assigned"),
+
 ]
