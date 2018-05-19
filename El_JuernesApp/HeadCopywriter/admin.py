5c21d207f6d89c75356bb89dab9163956a0cdef8
adricasi <adriator6@gmail.com>
2018-05-19 16:06:50 +0200

    El redactor cap visualitza les imatges d'un article a validar

--- El_JuernesApp/HeadCopywriter/admin.py
+++ El_JuernesApp/HeadCopywriter/admin.py
@@ -5,3 +5,4 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Article_comentat)
+admin.site.register(Images_sended)
--- El_JuernesApp/HeadCopywriter/models.py
+++ El_JuernesApp/HeadCopywriter/models.py
@@ -1,6 +1,9 @@
 # Create your models here.
 from django.db import models
 
+from AfeNews.models import New
+from Graphic_reporter.models import Image
+
 
 class Article_comentat(models.Model):
     slug = models.CharField(max_length=140)
@@ -8,3 +11,11 @@ class Article_comentat(models.Model):
 
     def __str__(self):
         return 'Article: ' + self.slug
+
+
+class Images_sended(models.Model):
+    noticia = models.ForeignKey(New, on_delete=models.CASCADE)
+    images = models.ManyToManyField(Image)
+
+    def __str__(self):
+        return 'Image_Request: ' + self.noticia.title
